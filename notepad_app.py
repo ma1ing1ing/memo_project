7c15ce11a43a72ca99afd9d211503b2b6f54b68a
ma1ing1ing <m141414alin@gmail.com>
2025-07-02 16:39:15 +0900

    코드 수정 : CLI부터 시작

--- notepad_app.py
+++ notepad_app.py
@@ -1,9 +1,19 @@
-import tkinter as tk
+def save_note(filename, contet):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write(contet)
 
-root = tk.Tk()
-root.title("마린이의 메모장")
-root.geometry("600x300")
+def load_note(filename):
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        return f.read()
+    
+filename = input("파일 이름을 저장하세요 : ")
+action = input("저장하려면 w, 불러오려면 r 입력 : ")
 
-text_area = tk.Text(root, font=("Helvectia", 12))
+if action == 'w':
+    content = input("저장할 내용을 입력하세요 \n: ")
+    save_note(filename, content)
+    print("저장 완료!")
+elif action == 'r' :
+    print("내용:\n", load_note(filename))
 
-root.mainloop()
+    
